e5bb7dc6d5460c555383437c830d1c7ae62853d6
C4r4cole <c4r4cole.chevalerie@gmail.com>
2023-10-06 19:03:16 +0200

    first 2 steps done

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,26 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+  const { name, portrait, city, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    const h3 = document.createElement("h3");
+    h3.textContent = city;
+    const paragraphe1 = document.createElement("p");
+    paragraphe1.textContent = tagline;
+    const paragraphe2 = document.createElement("p");
+    paragraphe2.textContent = `${price}€/jour`;
+    article.appendChild(img);
+    article.appendChild(h2);
+    article.appendChild(h3);
+    article.appendChild(paragraphe1);
+    article.appendChild(paragraphe2);
+    return article;
+  }
+  return { name, picture, getUserCardDOM };
 }
