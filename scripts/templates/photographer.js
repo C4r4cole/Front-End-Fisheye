0e1099370bc492f862eacc4ad2e9644bdca6d458
C4r4cole <c4r4cole.chevalerie@gmail.com>
2023-10-13 11:38:20 +0200

    étape 3 en cours

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -5,18 +5,33 @@ function photographerTemplate(data) {
 
   function getUserCardDOM() {
     const article = document.createElement("article");
+    const focusedArea = document.createElement("div");
+    focusedArea.classList.add("focused-area");
     const img = document.createElement("img");
     img.setAttribute("src", picture);
+    img.setAttribute("alt", "image du photographe");
     const h2 = document.createElement("h2");
     h2.textContent = name;
+    h2.setAttribute("aria-label", `nom du photographe : ${name}`);
     const h3 = document.createElement("h3");
     h3.textContent = city;
+    h3.setAttribute("aria-label", `ville du photographe : ${city}`);
     const paragraphe1 = document.createElement("p");
     paragraphe1.textContent = tagline;
+    paragraphe1.setAttribute(
+      "aria-label",
+      `citation du photographe : ${tagline}`
+    );
     const paragraphe2 = document.createElement("p");
     paragraphe2.textContent = `${price}€/jour`;
-    article.appendChild(img);
-    article.appendChild(h2);
+    paragraphe2.classList.add("price");
+    paragraphe2.setAttribute(
+      "aria-label",
+      `prix de la prestation du photographe : ${price}€/jour`
+    );
+    article.appendChild(focusedArea);
+    focusedArea.appendChild(img);
+    focusedArea.appendChild(h2);
     article.appendChild(h3);
     article.appendChild(paragraphe1);
     article.appendChild(paragraphe2);
