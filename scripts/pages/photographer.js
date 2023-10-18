f0c33c1fdac1171da8303d546333a471cc5a0eaa
C4r4cole <c4r4cole.chevalerie@gmail.com>
2023-10-18 18:45:39 +0200

    étape 4 ongoing

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,68 @@
 //Mettre le code JavaScript lié à la page photographer.html
+(async function () {
+  let params = new URL(document.location).searchParams;
+  let id = parseInt(params.get("id"));
+
+  console.log(id);
+
+  async function photographerCardLeft() {
+    const response = await fetch("data/photographers.json");
+    const data = await response.json();
+
+    const photographers = data.photographers;
+
+    const photographer = photographers.find((item) => item.id === id);
+
+    if (photographer) {
+      const { name, city, tagline } = photographer;
+
+      const article = document.createElement("article");
+      const h2 = document.createElement("h2");
+      h2.textContent = name;
+      const h3 = document.createElement("h3");
+      h3.textContent = city;
+      const p = document.createElement("p");
+      p.textContent = tagline;
+
+      article.appendChild(h2);
+      article.appendChild(h3);
+      article.appendChild(p);
+      return article;
+    }
+  }
+
+  async function photographerCardRight() {
+    const response = await fetch("data/photographers.json");
+    const data = await response.json();
+
+    const photographers = data.photographers;
+
+    const photographer = photographers.find((item) => item.id === id);
+
+    if (photographer) {
+      const { portrait } = photographer;
+
+      const picture = `assets/photographers/${portrait}`;
+
+      const article = document.createElement("article");
+      const img = document.createElement("img");
+      img.setAttribute("src", picture);
+      img.setAttribute("alt", "");
+      img.classList.add("profile-pic");
+
+      article.appendChild(img);
+      return article;
+    }
+  }
+
+  const photographHeaderLeft = document.querySelector(
+    ".photograph-header_left"
+  );
+  const photographHeaderRight = document.querySelector(
+    ".photograph-header_right"
+  );
+  const photographerArticleLeft = await photographerCardLeft();
+  const photographerArtcileRight = await photographerCardRight();
+  photographHeaderLeft.appendChild(photographerArticleLeft);
+  photographHeaderRight.appendChild(photographerArtcileRight);
+})();
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,37 +1,29 @@
 function photographerTemplate(data) {
-  const { name, portrait, city, tagline, price } = data;
+  const { name, portrait, city, tagline, price, id } = data;
 
   const picture = `assets/photographers/${portrait}`;
 
   function getUserCardDOM() {
     const article = document.createElement("article");
-    const focusedArea = document.createElement("div");
-    focusedArea.classList.add("focused-area");
+    const lien = document.createElement("a");
+    lien.href = `photographer.html?id=${id}`;
+    lien.classList.add("focused-area");
     const img = document.createElement("img");
     img.setAttribute("src", picture);
-    img.setAttribute("alt", "image du photographe");
+    img.setAttribute("alt", "");
     const h2 = document.createElement("h2");
     h2.textContent = name;
-    h2.setAttribute("aria-label", `nom du photographe : ${name}`);
     const h3 = document.createElement("h3");
     h3.textContent = city;
-    h3.setAttribute("aria-label", `ville du photographe : ${city}`);
     const paragraphe1 = document.createElement("p");
     paragraphe1.textContent = tagline;
-    paragraphe1.setAttribute(
-      "aria-label",
-      `citation du photographe : ${tagline}`
-    );
     const paragraphe2 = document.createElement("p");
     paragraphe2.textContent = `${price}€/jour`;
     paragraphe2.classList.add("price");
-    paragraphe2.setAttribute(
-      "aria-label",
-      `prix de la prestation du photographe : ${price}€/jour`
-    );
-    article.appendChild(focusedArea);
-    focusedArea.appendChild(img);
-    focusedArea.appendChild(h2);
+
+    article.appendChild(lien);
+    lien.appendChild(img);
+    lien.appendChild(h2);
     article.appendChild(h3);
     article.appendChild(paragraphe1);
     article.appendChild(paragraphe2);
