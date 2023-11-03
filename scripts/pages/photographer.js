fdcc593c6d408fedeb3861c2a40f1bda85573bd9
C4r4cole <c4r4cole.chevalerie@gmail.com>
2023-11-03 18:23:20 +0100

    grid almost done. Split images or videos fixed

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -72,27 +72,73 @@
 
     const photos = data.media;
 
-    const photo = photos.find((item) => item.photographerId === id);
+    const photographerPhotos = photos.filter(
+      (item) => item.photographerId === id
+    );
 
-    if (photo) {
-      const { id, photographerId, title, image, likes, date, price } = photo;
+    if (photographerPhotos.length > 0) {
+      // Vérifie s'il y a des photos
+      const photoGrid = document.createElement("article");
+      photoGrid.classList.add("photo-grid");
 
-      const picture = `assets/images/${photographerId}/${image}`;
+      photographerPhotos.forEach((photo) => {
+        const { id, photographerId, title, image, video, likes, date, price } =
+          photo;
 
-      const article = document.createElement("article");
-      const img = document.createElement("img");
-      img.setAttribute("src", picture);
-      img.setAttribute("alt", "");
-      img.classList.add("artist-pic");
+        const card = document.createElement("div");
+        const legend = document.createElement("div");
+        const titre = document.createElement("h2");
+        const likesDiv = document.createElement("div");
+        const coeur = document.createElement("div");
+        const icon = document.createElement("i");
 
-      console.log(article);
+        // Déterminez si c'est une vidéo ou une image
+        if (video) {
+          const videoElement = document.createElement("video");
+          videoElement.setAttribute("controls", "true");
+          videoElement.classList.add("artist-video");
 
-      article.appendChild(img);
-      return article;
+          const sourceElement = document.createElement("source");
+          sourceElement.setAttribute(
+            "src",
+            `assets/images/${photographerId}/${video}`
+          );
+          sourceElement.setAttribute("type", "video/mp4");
+
+          videoElement.appendChild(sourceElement);
+          card.appendChild(videoElement);
+        } else if (image) {
+          const img = document.createElement("img");
+          img.setAttribute("src", `assets/images/${photographerId}/${image}`);
+          img.setAttribute("alt", "");
+          img.classList.add("artist-pic");
+          card.appendChild(img);
+        }
+
+        legend.classList.add("legend");
+        titre.textContent = title;
+        coeur.textContent = likes;
+        likesDiv.classList.add("likes-div");
+        coeur.classList.add("likes");
+        icon.classList.add("fa-solid", "fa-heart");
+
+        card.appendChild(legend);
+        legend.appendChild(titre);
+        legend.appendChild(likesDiv);
+        likesDiv.appendChild(coeur);
+        likesDiv.appendChild(icon);
+
+        photoGrid.appendChild(card);
+      });
+
+      return photoGrid;
     }
+
+    // Si aucune photo ou vidéo correspondante n'est trouvée
+    return null;
   }
 
-  const galleryGridImages = document.querySelector(".gallery");
+  const galleryGridImages = document.querySelector(".gallery-container");
   const galleryArticle = await galleryGrid();
   galleryGridImages.appendChild(galleryArticle);
 })();
